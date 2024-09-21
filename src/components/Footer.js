c8e54d8b150d951d5730c24ddefd50955dc6fa22
gitprafulvaghani <prafulvaghani999@gmail.com>
2024-09-21 11:19:48 +0530

    changes added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href=""
+                href="https://github.com/gitprafulvaghani"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
